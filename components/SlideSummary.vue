68cbebcb65ae45d121fcf17076896d660cada929
Nishida Michiya <m.nishida@sint.co.jp>
2025-06-27 06:55:39 +0900

    first commit

---
+++ components/SlideSummary.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+  content: string;
+}>();
+</script>
+
+<template>
+  <!-- 画面下の中央に配置 -->
+  <div class="text-xl font-bold absolute bottom-10 text-center">
+    {{ content }}
+  </div>
+</template>
