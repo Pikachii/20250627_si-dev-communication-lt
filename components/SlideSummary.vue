c92c1b8bc51e0c4b62a55314d16d013c7b8f104b
Nishida Michiya <m.nishida@sint.co.jp>
2025-06-27 07:27:46 +0900

    fix: SlideSummaryコンポーネントにスロットを追加し、内容を動的に表示 feat: スライドに強調表示されたテキストを追加 style: 新しいCSSファイルを作成し、ul要素のマージンを設定

--- components/SlideSummary.vue
+++ components/SlideSummary.vue
@@ -9,6 +9,6 @@ defineProps<{
 <template>
   <!-- 画面下の中央に配置 -->
   <div class="text-xl font-bold absolute bottom-10 text-center">
-    {{ content }}
+    <slot >{{ content }}</slot>
   </div>
 </template>
